81cd72c523fd54e5c9ccd44b8e066f4266996157
= <=>
2023-05-02 21:56:36 +0530

    Starting page is Implemented.

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,115 +1,123 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import 'package:drop_shadow/drop_shadow.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget
+{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: "University-BRAU",
+      debugShowCheckedModeBanner: false,
+      home : Scaffold(
+        body: Stack(
+            children: <Widget>[
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+              // Ellipse6
+              Positioned(
+                left : 33,
+                top : 60,
+                child : Image.asset('assets/images/Ellipse6.png'),
+              ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+              // Ellipse7
+              Positioned(
+                left : 75,
+                top : 100,
+                child: Image.asset('assets/images/Ellipse7.png'),
+              ),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+              // BRAU LOGO
+              Positioned(
+                left: 108,  //110
+                top: 130,   //130
+                child: Center(child: Image.asset('assets/images/logo.png')),
+              ),
+              // Text
+              Center(
+                child: SelectionArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const<Widget>
+                    [
+                      SizedBox(height:230),    // 230
+                      Center(
+                        child: Text('All you need is',
+                            style:TextStyle(
+                              fontFamily:'LibreFranklin-Bold',
+                              fontSize:35,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Text('at one place ',
+                          style:TextStyle(
+                            fontFamily:'LibreFranklin-Bold',
+                            fontSize:35,
+                            fontWeight: FontWeight.bold,)),
+                      SizedBox(
+                        height: 45,
+                      ),
+                      SelectionContainer.disabled(
+                          child: Text('Complete your task like drinking water,',
+                              style:TextStyle(
+                                fontFamily:'LibreFranklin-Light',
+                                fontSize:15, ))),(
+                          Text('no obstacle just flow into body.',
+                              style:TextStyle(
+                                fontFamily:'LibreFranklin-Light',
+                                fontSize:15,))),
+                    ],
+                  ),
+                ),
+              ),
 
-  final String title;
+              // Button
+              Positioned(
+                top: 620,       // 635
+                height: 59,
+                left: 20,
+                right: 20,
+                child:
+                SizedBox(
+                  height: 59,
+                  width: 325,
+                  child: Container(
+                      decoration:  BoxDecoration(
+                        shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(50),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Color(0x33000000),
+                                offset: Offset(4, 6),
+                                blurRadius: 4,
+                              spreadRadius: 0,
+                            ),
+                          ]
+                      ),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                            backgroundColor: const Color(0xFF00512D),   // #00512D
+                            padding: EdgeInsets.fromLTRB(76, 18, 76, 17),
+                          ),
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+                          onPressed: (){},  // Next Page Navigation.
+                          child : const Text(
+                            "Get started",
+                            style: TextStyle(
+                                fontFamily: "LibreFranklin-Regular",
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400
+                            ),
+                          ),
+                        ),
+                      ),
+                  ),
+                ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
